bb91f3fe528c87ae8404397616712eb4eba77a14
אור ישראלי <1948OR1967@GMAIL.COM>
2020-03-21 22:06:24 +0200

    fixed the ui a bit

--- Analyzer.py
+++ Analyzer.py
@@ -1,15 +1,16 @@
 import PyPDF2
 from gtts import gTTS
-def read_pdf_text(path,q):
+def read_pdf_text(path,retured_value):
     extracted_text = ""
-    with open(path,'rb') as file:
-        reader = PyPDF2.PdfFileReader(file)
-        for i in range(reader.numPages):
-            page = reader.getPage(i)
-            extracted_text = extracted_text + page.extractText()
-            precent = int(round(((i+1)/reader.numPages)*100))
-            print(f"reading at {precent}%")
-        q.put(extracted_text)
+    if path:
+        with open(path,'rb') as file:
+            reader = PyPDF2.PdfFileReader(file)
+            for i in range(reader.numPages):
+                page = reader.getPage(i)
+                extracted_text = extracted_text + page.extractText()
+                precent = int(round(((i+1)/reader.numPages)*100))
+                print(f"reading at {precent}%")
+            retured_value.put(extracted_text)
 
 def FormatToVoice(string_to_read,file_dest):
     tts = gTTS(string_to_read)
--- GuiHandler.py
+++ GuiHandler.py
@@ -1,48 +1,42 @@
-from tkinter import filedialog,messagebox
+from tkinter import filedialog, messagebox
+import tkinter.font as font
 import tkinter 
 import Analyzer
 import threading
 import queue
-class Threader(threading.Thread):
-####TODO: fix everything!
-    def __init__(self, *args, **kwargs):
-
-        threading.Thread.__init__(self, *args, **kwargs)
+import os
+class Worker(threading.Thread):
+    def __init__(self,label):
+        threading.Thread.__init__(self)
         self.daemon = True
+        self.label = label
         self.start()
 
     def run(self):
         file_name= tkinter.filedialog.askopenfilename(initialdir = "/",title = "Select a pdf",filetypes = (("pdf","*.pdf"),("all files","*.*")))
-        messagebox.showinfo("Title",f"analysing {file_name} this may take several minutes...")
-        q = queue.Queue()
-        Analyzer.read_pdf_text(file_name,q)
-        file_string =  q.get()
-        new_file_name = file_name.replace('.pdf','.mp3')
-        Analyzer.FormatToVoice(file_string,new_file_name)
-        tkinter.Label(text="analyzing pdf...",height=20).pack()
-
+        returned_value = queue.Queue()
+        self.label.config(text="analyzing pdf...",fg="green",height=20)
+        Analyzer.read_pdf_text(file_name, returned_value)
+        file_string =  returned_value.get()
+        new_file_name = file_name.replace('.pdf', '.mp3')
+        self.label.config(text=f"converting to mp3 in path {new_file_name}",fg="blue")
+        Analyzer.FormatToVoice(file_string, new_file_name)
+        os.startfile(new_file_name)
+ 
 
 def build_gui():
     main_window = tkinter.Tk() 
     main_window.geometry("400x400")
     main_window.configure(bg="#fffeee")
     main_window.title('please stand by...')
-    label = tkinter.Label(text=" please drop the pdf you want to be converted!",height=20)
-    open_file_dialog_button  =tkinter.Button(text="choose file!",fg="white",bg ="#0366d6",command = lambda:Threader())
+    label = tkinter.Label(text=" please drop the pdf you want to be converted!", height=20, bg="white")
+    button_font = font.Font(size=10)
+    open_file_dialog_button  =tkinter.Button(text="choose file!",fg="white",bg ="#0366d6",height=3,width=30,borderwidth = 0,command = lambda:Worker(label))
+    label["font"] = button_font
+    open_file_dialog_button["font"] = button_font
     label.pack()
     open_file_dialog_button.pack()
     main_window.mainloop()
-def open_file_dialog(label): 
-   ''' file_name= tkinter.filedialog.askopenfilename(initialdir = "/",title = "Select a pdf",filetypes = (("pdf","*.pdf"),("all files","*.*")))
-    messagebox.showinfo("Title",f"analysing {file_name} this may take several minutes...")
-    label.config(text=f"file path:{file_name}")
-    q = queue.Queue()
-    Threader(file_name=file_name,q=q)
-    file_string =  q.get()
-    new_file_name = file_name.replace('.pdf','.mp3')
-    Analyzer.FormatToVoice(file_string,new_file_name)
-    tkinter.Label(text="analyzing pdf...",height=20).pack()
 
-    return file_name
-    '''
 build_gui()
+
