af2ffa1b3e96e010438eea5d019ae7f632cf348b
unknown <1948or1967@gmaIL.COM>
2021-04-02 05:34:16 -0700

    minor changes and pdf library change

--- Analyzer.py
+++ Analyzer.py
@@ -1,19 +1,36 @@
-import PyPDF2
+from io import StringIO
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfinterp import resolve1
+
 import pyttsx3
 def read_pdf_text(path,retured_value):
-    extracted_text = ""
+    output_string = StringIO()
     if path:
         with open(path,'rb') as file:
-            reader = PyPDF2.PdfFileReader(file)
-            for i in range(reader.numPages):
-                page = reader.getPage(i)
-                extracted_text = extracted_text + page.extractText()
-                precent = int(round(((i+1)/reader.numPages)*100))
+            parser = PDFParser(file)
+            fileDoc = PDFDocument(parser)
+            rsrcmgr = PDFResourceManager()
+            device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+            interpreter = PDFPageInterpreter(rsrcmgr, device)
+            fileLen = resolve1(fileDoc.catalog['Pages'])['Count']
+            counter = 0
+            for page in PDFPage.create_pages(fileDoc):
+                
+                precent = int(round(((counter+1)/fileLen)*100))
                 print(f"reading at {precent}%")
-            retured_value.put(extracted_text)
+                interpreter.process_page(page)
+                counter+=1    
+    retured_value = output_string
+    
+    return retured_value
 
 def onEnd(name, completed):
-   print('finished!!!!')
+   print('done!')
 
 def FormatToVoice(string_to_read,file_dest):
     engine = pyttsx3.init()  # object creation
--- GuiHandler.py
+++ GuiHandler.py
@@ -16,23 +16,27 @@ class Worker(threading.Thread):
         file_name= tkinter.filedialog.askopenfilename(initialdir = "/",title = "Select a pdf",filetypes = (("pdf","*.pdf"),("all files","*.*")))
         returned_value = queue.Queue()
         self.label.config(text="analyzing pdf...",fg="#f5862f",height=20)
-        Analyzer.read_pdf_text(file_name, returned_value)
-        file_string =  returned_value.get()
-        new_file_name = file_name.replace('.pdf', '.mp3')
-        self.label.config(text=f"converting to mp3 in path {new_file_name}",fg="blue")
-        Analyzer.FormatToVoice(file_string, new_file_name)
-        self.label.config(text=f"done! you can hear the file now!",fg="green")
-        os.startfile(new_file_name)
- 
+        file_strio = Analyzer.read_pdf_text(file_name, returned_value)
+        file_string =  file_strio.getvalue()
+        if file_string:
+            new_file_name = file_name.replace('.pdf', '.mp3')
+            self.label.config(text=f"converting to mp3 in path {new_file_name}",fg="blue")
+            Analyzer.FormatToVoice(file_string, new_file_name)
+            self.label.config(text=f"done! you can hear the file now!",fg="green")
+            os.startfile(new_file_name)
+        else:
+            self.label.config(text=f"Couldn't format file 😒",fg="red")
+
+    
 
 def build_gui():
     main_window = tkinter.Tk() 
-    main_window.geometry("400x400")
+    main_window.geometry("500x500")
     main_window.configure(bg="#fffeee")
     main_window.title('pdf to speech')
-    label = tkinter.Label(text=" please drop the pdf you want to be converted!", height=20, bg="white")
-    button_font = font.Font(size=10)
-    open_file_dialog_button  =tkinter.Button(text="choose file!",fg="white",bg ="#0366d6",height=3,width=30,borderwidth = 0,command = lambda:Worker(label))
+    label = tkinter.Label(text=" please drop the pdf you want to be converted!", height=20, bg="#fffeee")
+    button_font = font.Font(size=12)
+    open_file_dialog_button  =tkinter.Button(text="choose file!",fg="#fffeee",bg ="green",height=3,width=30,borderwidth = 0,command = lambda:Worker(label))
     label["font"] = button_font
     open_file_dialog_button["font"] = button_font
     label.pack()
